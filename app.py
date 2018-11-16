0f1ee8a2dac15b16cd743e82139c1a2ef2589520
Troy Swanson <swanson.troy@heb.com>
2018-11-16 09:40:54 -0600

    Refactor to use function for moon phase iterable

--- app.py
+++ app.py
@@ -15,13 +15,26 @@ from datetime import date, timedelta
 
 # CONSTANTS
 MOON_PHASES = [
-    ":new_moon: New Moon",
-    ":first_quarter_moon: First Quarter Moon",
-    ":full_moon: Full Moon",
-    ":last_quarter_moon: Last Quarter Moon"
+    emoji.emojize(":new_moon: New Moon"),
+    emoji.emojize(":first_quarter_moon: First Quarter Moon"),
+    emoji.emojize(":full_moon: Full Moon"),
+    emoji.emojize(":last_quarter_moon: Last Quarter Moon")
 ]
 CAL_NAME = "Lunar Phases"
 CAL_COLOR = "#003153"
+
+# MOON PHASE FUNCTION
+def moon_phase_list(t0, t1):
+    t, p = almanac.find_discrete(t0, t1, almanac.moon_phases(e))
+
+    l = []
+    for i in range(0, len(t)):
+        l.append({
+            't': t[i].utc_datetime(),
+            'p': p[i]
+        })
+
+    return l
 
 @app.route("/")
 def cal():
@@ -33,9 +46,6 @@ def cal():
     t0 = ts.utc(today - 2 * year)
     t1 = ts.utc(today + 2 * year)
 
-    # get moon phases for time interval
-    t, y = almanac.find_discrete(t0, t1, almanac.moon_phases(e))
-
     # create calendar object
     cal = Calendar()
     cal.add('dtstart', t0.utc_datetime().date())
@@ -44,10 +54,10 @@ def cal():
     cal.add('x-apple-calendar-color', CAL_COLOR)
 
     # create event objects
-    for i in range(0, len(t)):
+    for i in moon_phase_list(t0, t1):
         event = Event()
-        event.add('dtstart', t[i].utc_datetime().date())
-        event.add('summary', emoji.emojize(MOON_PHASES[y[i]]))
+        event.add('dtstart', i.get('t').date()) # using a date object for dtstart infers an all day event
+        event.add('summary', MOON_PHASES[i.get('p')])
         cal.add_component(event)
 
     # generate response
